cd71b6f105921db4cd8dcadb10a3bce0ed40d6ed
Snowy7 <snowydev7@gmail.com>
2023-07-15 16:21:24 +0300

    Fixed GroupMention + Mention All for Aires

--- src/utils/Commands.js
+++ src/utils/Commands.js
@@ -82,7 +82,7 @@ async function GroupMention(msg, client = null, includeAdmins = false) {
     console.log(includeAdmins) // (user.admin === "admin" || user.admin === "superadmin")
     const mentions = msg.groupMetadata.participants.filter((user) => {
         if (!includeAdmins) {
-            return !(user.admin === "admin" || user.admin === "superadmin") && user.id !== msg.key.remoteJid
+            return user.admin !== "admin" && user.admin !== "superadmin" && user.id !== msg.key.remoteJid
         } else {
             return user.id !== msg.key.remoteJid
         }
