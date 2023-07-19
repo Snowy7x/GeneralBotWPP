d18357978e2b8d3650ae8a60c321819e7624df92
Snowy7 <snowydev7@gmail.com>
2023-07-19 20:26:15 +0300

    Fixed the ChatBot

--- src/Controllers/ChatBotController.js
+++ src/Controllers/ChatBotController.js
@@ -1,7 +1,9 @@
 import Bard from "bard-ai";
 
 let newConversation = null
-Bard.init("YgglpsO7Tk3rbswYjGJqTPu0lFlzbS0xnhyXMOIUytN90PKXiXr8Sil6HytrhbPJSjKAJg.").then(() => {
+// YgglpsO7Tk3rbswYjGJqTPu0lFlzbS0xnhyXMOIUytN90PKXiXr8Sil6HytrhbPJSjKAJg.
+// YwhObmjaTcyw2p8qudC5M9-mkZczMQP4uwWd-OSEdWHo6wr7J4NrnXY-ansDkEwIsNiHMw.
+Bard.init("YwhObmjaTcyw2p8qudC5M9-mkZczMQP4uwWd-OSEdWHo6wr7J4NrnXY-ansDkEwIsNiHMw.").then(() => {
     newConversation = new Bard.Chat()
     newConversation.ask("You will be Snowy and you will answer only short answers only. respond with 'Ok' if you are ready").then((re) => {
         console.log("Bard is ready to chat!: " + re)
